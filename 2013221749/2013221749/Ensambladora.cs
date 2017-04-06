2ddfb9ad240d5df1836afd62e3f8b0ffa3a31521
Clase <Clase>
2017-04-05 19:18:52 -0500

    cuarto commit

--- 2013221749/2013221749/Ensambladora.cs
+++ 2013221749/2013221749/Ensambladora.cs
@@ -8,5 +8,13 @@ namespace _2013221749
 {
     class Ensambladora
     {
+        private Carro _carro;
+
+        public Ensambladora(Carro carro)
+        {
+            _carro = carro;
+        }
+
+        public Carro Carro { get { return _carro; } }
     }
 }
